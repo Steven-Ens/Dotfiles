e41b8babce4e257b6cffed799683fb1d6b62c8ac
Steven-Ens <84874430+Steven-Ens@users.noreply.github.com>
2026-06-22 13:15:24 -0700

    Update path names to match update script

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -4,31 +4,33 @@
 set -e
 
 USER_HOME="/home/steve"
-VIM_PACK="$USER_HOME/.vim/pack/plugins/start"
-VIM_COLORS="$USER_HOME/.vim/colors"
+# Vim plugin directory.
+PLUGIN_DIR="$USER_HOME/.vim/pack/plugins/start"
+# Vim colorscheme directory.
+COLOR_DIR="$USER_HOME/.vim/colors"
 
-mkdir -p "$VIM_PACK"
-mkdir -p "$VIM_COLORS"
+mkdir -p "$PLUGIN_DIR"
+mkdir -p "$COLOR_DIR"
 
 # Vim plugins
-git clone https://github.com/Raimondi/delimitMate.git "$VIM_PACK/delimitMate" || true
-git clone https://github.com/Yggdroot/indentLine.git "$VIM_PACK/indentLine" || true
-git clone https://github.com/preservim/nerdtree.git "$VIM_PACK/nerdtree" || true
-git clone https://github.com/preservim/tagbar.git "$VIM_PACK/tagbar" || true
-git clone https://tpope.io/vim/fugitive.git "$VIM_PACK/vim-fugitive" || true
-git clone https://tpope.io/vim/repeat.git "$VIM_PACK/vim-repeat" || true
-git clone https://tpope.io/vim/surround.git "$VIM_PACK/vim-surround" || true
-git clone --branch release https://github.com/neoclide/coc.nvim.git "$VIM_PACK/coc.nvim" || true
+git clone https://github.com/Raimondi/delimitMate.git "$PLUGIN_DIR/delimitMate" || true
+git clone https://github.com/Yggdroot/indentLine.git "$PLUGIN_DIR/indentLine" || true
+git clone https://github.com/preservim/nerdtree.git "$PLUGIN_DIR/nerdtree" || true
+git clone https://github.com/preservim/tagbar.git "$PLUGIN_DIR/tagbar" || true
+git clone https://tpope.io/vim/fugitive.git "$PLUGIN_DIR/vim-fugitive" || true
+git clone https://tpope.io/vim/repeat.git "$PLUGIN_DIR/vim-repeat" || true
+git clone https://tpope.io/vim/surround.git "$PLUGIN_DIR/vim-surround" || true
+git clone --branch release https://github.com/neoclide/coc.nvim.git "$PLUGIN_DIR/coc.nvim" || true
 
 # Install coc.nvim
-cd "$VIM_PACK/coc.nvim"
+cd "$PLUGIN_DIR/coc.nvim"
 npm ci
 
 # Install Solidity language server for coc.nvim
 npm install -g @nomicfoundation/solidity-language-server
 
-# Nord Vim theme
-git clone https://github.com/nordtheme/vim.git "$VIM_PACK/nord-vim" || true
-cp "$VIM_PACK/nord-vim/colors/nord.vim" "$VIM_COLORS/nord.vim"
+# Install Nord Vim theme
+git clone https://github.com/nordtheme/vim.git "$PLUGIN_DIR/nord-vim" || true
+cp "$PLUGIN_DIR/nord-vim/colors/nord.vim" "$COLOR_DIR/nord.vim"
 
 echo "Vim plugins and Solidity language server installed successfully."
